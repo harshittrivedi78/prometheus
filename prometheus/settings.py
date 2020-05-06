38ebf5e259acf1f904f77f3573369d34e618d378
Harshit <harshit@INODE07357.local>
2020-05-06 21:17:42 +0530

    modified gitignore and setting up prometheus message and field name

--- prometheus/settings.py
+++ prometheus/settings.py
@@ -3,14 +3,15 @@ from django.conf import settings
 
 # Prometheus Settings
 
-MULTIPROC_MODE = getattr(settings, "MULTIPROC_MODE", True)
-MULTIPROC_DIR = settings.BASE_DIR + '/prometheus/multiproc_dir'
+PROMETHEUS_MULTIPROC_MODE = getattr(settings, "PROMETHEUS_MULTIPROC_MODE", True)
+PROMETHEUS_MULTIPROC_DIR = getattr(settings, "PROMETHEUS_MULTIPROC_DIR",
+                                   settings.BASE_DIR + '/prometheus/multiproc_dir')
 
 
 def setup():
-    print("setting up prometheus")
-    if MULTIPROC_MODE:
-        dir_name = MULTIPROC_DIR
+    print("Setting up prometheus")
+    if PROMETHEUS_MULTIPROC_MODE:
+        dir_name = PROMETHEUS_MULTIPROC_DIR
         if not os.path.exists(dir_name):
             os.makedirs(dir_name)
         os.environ["prometheus_multiproc_dir"] = dir_name
